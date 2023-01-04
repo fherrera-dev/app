964b00d7c4a6e6787c53b99324edaa9799ec5b7a
fherrera <dev.fherrera@gmail.com>
2023-01-03 20:55:33 -0600

    pruebas

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload holaaa.
+          Edit <code>src/App.js</code> and save to reload holaaa otra vez.
         </p>
         <a
           className="App-link"
