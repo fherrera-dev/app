084625843a86429a8de2a6e80e8cd2957cb08c9a
fherrera <dev.fherrera@gmail.com>
2023-01-09 19:36:34 -0600

    contenido del home

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,45 +1,173 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-
+import imgSlot from '../assets/img-perezosos-sloth-filivora.jpg';
+import imgKayak from '../assets/img-tour-kayak.jpg';
+import imgRafting from '../assets/img-tour-rapidos-rafting.jpg';
+import imgHots from '../assets/img-tour-termales-hotsprinig.jpg';
+import imgCararata from '../assets/img-tour-catarata-waterfall.jpg';
+import imgPedal from '../assets/img-tour-peddling.jpg';
 class Home extends Component {
 
-    componentDidMount(){
+    /*componentDidMount(){
         axios.get('https://jsonplaceholder.typicode.com/users')
         .then(data=>{
             console.log(data);
         })
-    }
+    }*/
+
+    //imgSlot = require("./assets/imgSlot.jpg").default;
     
     state = {  } 
     render() { 
         return (
-            <div class="card-group mt-5">
-                <div class="card">
-                    
+            <main class='container'>
+               
+            <div class="row row-cols-1 row-cols-md-3 margen">
+                <div class="col mb-4 card-tour">
+                    <img src={imgSlot} />
                     <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                        <h6 class="card-title">Sloth Tour</h6>
+                        <p class="card-text"><small class="text-muted">Includes transpor, entrance and Meal, tour guide</small></p>
+                        
+                        {/* nuevo cod */}
+                        <div>
+                            <a><h4>SEE INFO...</h4></a>
+                        </div>
+
+                        <div class="tour-book">
+                            <div class="section-book">
+                                <a class="btn btn-success btn-book">Book</a>
+                            </div>
+
+
+                            <div class="section-price">
+                            <a class="btn btn-danger btn-price"><strong>$50</strong></a>
+                            </div>
+
+                        </div>
+                    
                     </div>
                 </div>
-                <div class="card">
-                    
+                <div class="col mb-4 card-tour">
+                <img src={imgKayak} />
                     <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                        <h6 class="card-title">Kayaking Tour</h6>
+                        <p class="card-text"><small class="text-muted">Includes transpor, entrance and Meal, tour guide</small></p>
+                    
+                        <div>
+                            <a><h4>SEE INFO...</h4></a>
+                        </div>
+
+                        <div class="tour-book">
+                            <div class="section-book">
+                                <a class="btn btn-success btn-book">Book</a>
+                            </div>
+
+
+                            <div class="section-price">
+                            <a class="btn btn-danger btn-price"><strong>$60</strong></a>
+                            </div>
+
+                        </div>
+                    
                     </div>
                 </div>
-                <div class="card">
-                    
+                <div class="col mb-4 card-tour">
+                <img src={imgRafting} />
                     <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                        <h6 class="card-title">Tour Rafting class 2 and 3</h6>
+                        <p class="card-text"><small class="text-muted">Includes transpor, entrance and Meal, profesional tour guide</small></p>
+
+                        <div>
+                            <a><h4>SEE INFO...</h4></a>
+                        </div>
+
+                        <div class="tour-book">
+                            <div class="section-book">
+                                <a class="btn btn-success btn-book">Book</a>
+                            </div>
+
+                            <div class="section-price">
+                            <a class="btn btn-danger btn-price"><strong>$100</strong></a>
+                            </div>
+
+                        </div>
+
+                    </div>
+                </div>
+            
+           
+                <div class="col mb-4 card-tour">
+                    <img src={imgHots} />
+                    <div class="card-body">
+                        <h6 class="card-title">Hot Springs Paradise</h6>
+                        <p class="card-text"><small class="text-muted">Includes transpor, entrance and Meal</small></p>
+                    
+                        <div>
+                            <a><h4>SEE INFO...</h4></a>
+                        </div>
+
+                        <div class="tour-book">
+                            <div class="section-book">
+                                <a class="btn btn-success btn-book">Book</a>
+                            </div>
+
+                            <div class="section-price">
+                            <a class="btn btn-danger btn-price"><strong>$40</strong></a>
+                            </div>
+
+                        </div>
+                    
+                    </div>
+                </div>
+                <div class="col mb-4 card-tour">
+                <img src={imgCararata} />
+                    <div class="card-body">
+                        <h6 class="card-title">Tour Waterfall</h6>
+                        <p class="card-text"><small class="text-muted">Includes transpor, entrance and Meal, tour guide</small></p>
+                    
+                        <div>
+                            <a><h4>SEE INFO...</h4></a>
+                        </div>
+
+                        <div class="tour-book">
+                            <div class="section-book">
+                                <a class="btn btn-success btn-book">Book</a>
+                            </div>
+
+                            <div class="section-price">
+                            <a class="btn btn-danger btn-price"><strong>$55</strong></a>
+                            </div>
+
+                        </div>
+                    
+                    </div>
+                </div>
+                <div class="col mb-4 card-tour">
+                <img src={imgPedal} />
+                    <div class="card-body">
+                        <h6 class="card-title">Tour Stand Up Paddling</h6>
+                        <p class="card-text"><small class="text-muted">Includes transpor, entrance and Meal, tour guide</small></p>
+                    
+                        <div>
+                            <a><h4>SEE INFO...</h4></a>
+                        </div>
+
+                        <div class="tour-book">
+                            <div class="section-book">
+                                <a class="btn btn-success btn-book">Book</a>
+                            </div>
+
+                            <div class="section-price">
+                            <a class="btn btn-danger btn-price"><strong>$75</strong></a>
+                            </div>
+
+                        </div>
                     </div>
                 </div>
             </div>
-    
+
+            </main>
         );
     }
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,42 +1,57 @@
 import React, { Component } from 'react';
+import logoSloth from '../assets/logo-sloth.png'
+
+
+
+
+
 
 class Navbar extends Component{
 
     render(){
 
         return(
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                <a className="navbar-brand" href="#">My APP</a>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+                <a className="navbar-brand" href="#">
+                    <img className="logo" src={logoSloth}/>
+                </a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
 
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav mr-auto">
-                    <li className="nav-item active">
-                        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Link</a>
-                    </li>
-                    <li className="nav-item dropdown">
-                        <a className="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
-                        Dropdown
-                        </a>
-                        <div className="dropdown-menu">
-                        <a className="dropdown-item" href="#">Action</a>
-                        <a className="dropdown-item" href="#">Another action</a>
-                        <div className="dropdown-divider"></div>
-                        <a className="dropdown-item" href="#">Something else here</a>
-                        </div>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link disabled">Disabled</a>
-                    </li>
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Booking</a>
+                        </li>
+                        <li className="nav-item dropdown">
+                            <a className="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
+                            Tours
+                            </a>
+                            <div className="dropdown-menu">
+                            <a className="dropdown-item" href="#">Extremos</a>
+                            <a className="dropdown-item" href="#">Aventura</a>
+                            <div className="dropdown-divider"></div>
+                            <a className="dropdown-item" href="#">Familiares</a>
+                            </div>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link">About</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link">Contact Us</a>
+                        </li>
                     </ul>
-                    <form className="form-inline my-2 my-lg-0">
-                   
+                    
+                    <div>
+                    <form class="form-inline my-2 my-lg-0">
+                        <input class="form-control mr-sm-2" type="search" placeholder="Search a tour" aria-label="Search"/>
+                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
                     </form>
+                    </div>
                 </div>
             </nav>
         )
